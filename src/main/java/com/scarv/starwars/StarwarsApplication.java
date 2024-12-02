a12c06ea90cb7f89b2fdac3b2e177d4da2f1c998
scarv <fernando.carvalho@lcsistemas.com.br>
2024-12-02 10:57:24 -0300

    chore: commit inicial

---
+++ src/main/java/com/scarv/starwars/StarwarsApplication.java
@@ -0,0 +1,13 @@
+package com.scarv.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsApplication.class, args);
+	}
+
+}
